6c91da5d33e6dbf9ba4b9d7427a0d09973258ffd
hyunseung <ardimento22@naver.com>
2025-01-11 23:24:26 +0900

    init SpringIntegration Client Project

---
+++ SpringIntegration/Client/src/main/java/springIntegration/ClientApplication.java
@@ -0,0 +1,15 @@
+package springIntegration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
