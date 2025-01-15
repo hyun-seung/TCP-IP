12e8c71e268139f7b03071127a4753df7a2aab93
hyunseung <ardimento22@naver.com>
2025-01-15 19:45:05 +0900

    [FEAT] Netty - Server :: Connect by byte[]

--- Netty/Server/src/main/java/server/config/NettyServerConfig.java
+++ Netty/Server/src/main/java/server/config/NettyServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import server.service.NettyServer;
 import server.service.NettyServer_v1;
+import server.service.NettyServer_v2;
 
 @Slf4j
 @Configuration
@@ -12,8 +13,8 @@ public class NettyServerConfig {
 
     @Bean
     public NettyServer create() {
-        NettyServer nettyServer = new NettyServer_v1();
-        log.info("{} running", nettyServer.getClass().getSimpleName());
+        NettyServer nettyServer = new NettyServer_v2();
+        log.info("{} started on port {}", nettyServer.getClass().getSimpleName(), ServerConfig.PORT);
         return nettyServer;
     }
 }
---
+++ Netty/Server/src/main/java/server/handler/ByteBufToByteArrayDecoder.java
@@ -0,0 +1,17 @@
+package server.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class ByteBufToByteArrayDecoder extends ByteToMessageDecoder {
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        byte[] bytes = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(bytes);
+
+        list.add(bytes);
+    }
+}
