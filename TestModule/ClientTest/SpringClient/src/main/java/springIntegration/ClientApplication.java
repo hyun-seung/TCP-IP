668d6e8d57970d0cb39a5738f943e08b642c27dd
hyunseung <ardimento22@naver.com>
2025-01-20 00:04:19 +0900

    init TestModule - SpringIntegration Client Project

---
+++ TestModule/ClientTest/SpringClient/src/main/java/springIntegration/ClientApplication.java
@@ -0,0 +1,14 @@
+package springIntegration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
